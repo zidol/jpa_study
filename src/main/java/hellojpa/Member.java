014b29997f99f0d55edfef6051e685409f886f1b
zidol <ajh3166@naver.com>
2020-09-18 00:48:17 +0900

    jpa insert 정석 코드

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,32 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name = "USER") // 테이블 이름 지정
+public class Member {
+
+    @Id
+//    @Column(name = "usnerId") //컬러몀 지정
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
